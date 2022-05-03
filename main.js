82c231107eb049ce795060c1fdd7bb5cbf5ad519
alexlearning77 <alex_learning@hotmail.com>
2022-05-03 10:29:35 +0200

    Nav work

---
+++ main.js
@@ -0,0 +1,15 @@
+const primaryNav = document.getElementById('prim_nav');
+const navToggle = document.getElementById('mobile_nav_toggle');
+
+navToggle.addEventListener("click", () => {
+
+    const visibility = primaryNav.getAttribute("data-visible");
+
+    if (visibility === 'false') {
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('area-expanded', true)
+    } else if (visibility === 'true') {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('area-expanded', false)
+    }
+});
